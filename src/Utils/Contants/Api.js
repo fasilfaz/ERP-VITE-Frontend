bae88dacff731c0c719b9021f893024774e822e6
Henna Maria <hennamaria2001@gmail.com>
2025-01-01 16:07:59 +0530

    items added

--- src/Utils/Contants/Api.js
+++ src/Utils/Contants/Api.js
@@ -1,6 +1,17 @@
 const baseUrl = import.meta.env.VITE_BASE_URL;
+
+// auth
 const loginEndpoint = import.meta.env.VITE_LOGIN_URL;
 const registerEndpoint = import.meta.env.VITE_REGISTER_URL;
 
+// items
+const getAllItemsEndpoint = import.meta.env.VITE_GET_ALL_ITEMS_URL;
+
+// =============================================================================================================
+
+// auth
 export const LOGIN_API = baseUrl + loginEndpoint;
 export const REGISTER_API = baseUrl + registerEndpoint;
+
+// items
+export const GET_ALL_ITEMS_API = baseUrl + getAllItemsEndpoint;
